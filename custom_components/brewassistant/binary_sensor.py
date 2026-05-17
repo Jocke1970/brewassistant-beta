22c336ea1b3ce7e0b082a38a2bde66648f85e6d1
Joachim Eriksson <sa0azy@gmail.com>
2026-05-17 15:09:20 +0200

    Expose smart recommendation binary sensors

--- custom_components/brewassistant/binary_sensor.py
+++ custom_components/brewassistant/binary_sensor.py
@@ -13,6 +13,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .const import DOMAIN
 from .coordinator import BrewAssistantCoordinator, BrewAssistantData
 from .entity import BrewAssistantEntity
+from .smart_recommendations import build_smart_recommendations
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -20,6 +21,21 @@ class BrewAssistantBinarySensorDescription(BinarySensorEntityDescription):
     """Describes a BrewAssistant binary sensor."""
 
     value_fn: Callable[[BrewAssistantData], bool]
+
+
+def _smart_data(coordinator: BrewAssistantCoordinator):
+    data = coordinator.data
+    if data is None:
+        return None
+    return build_smart_recommendations(
+        coordinator.hass,
+        liquid_temp=data.liquid_temperature,
+        target_temp=data.recipe_target_temperature,
+        delta=data.temperature_delta,
+        chamber_temp=data.chamber_temperature,
+        fallback_active=data.fallback_active,
+        source=data.liquid_temperature_source,
+    )
 
 
 BINARY_SENSORS: tuple[BrewAssistantBinarySensorDescription, ...] = (
@@ -35,7 +51,42 @@ BINARY_SENSORS: tuple[BrewAssistantBinarySensorDescription, ...] = (
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
         value_fn=lambda data: data.ready,
     ),
+    BrewAssistantBinarySensorDescription(
+        key="smart_heat_needed_core",
+        translation_key="smart_heat_needed_core",
+        value_fn=lambda data: False,
+    ),
+    BrewAssistantBinarySensorDescription(
+        key="smart_heat_permitted_core",
+        translation_key="smart_heat_permitted_core",
+        value_fn=lambda data: False,
+    ),
+    BrewAssistantBinarySensorDescription(
+        key="smart_cooling_recommended_core",
+        translation_key="smart_cooling_recommended_core",
+        value_fn=lambda data: False,
+    ),
+    BrewAssistantBinarySensorDescription(
+        key="smart_fan_recommended_core",
+        translation_key="smart_fan_recommended_core",
+        value_fn=lambda data: False,
+    ),
+    BrewAssistantBinarySensorDescription(
+        key="smart_rising_too_fast_core",
+        translation_key="smart_rising_too_fast_core",
+        device_class=BinarySensorDeviceClass.PROBLEM,
+        value_fn=lambda data: False,
+    ),
 )
+
+
+SMART_BINARY_KEYS = {
+    "smart_heat_needed_core",
+    "smart_heat_permitted_core",
+    "smart_cooling_recommended_core",
+    "smart_fan_recommended_core",
+    "smart_rising_too_fast_core",
+}
 
 
 async def async_setup_entry(
@@ -70,4 +121,18 @@ class BrewAssistantBinarySensor(BrewAssistantEntity, BinarySensorEntity):
         """Return whether the binary sensor is on."""
         if self.coordinator.data is None:
             return None
+        if self.entity_description.key in SMART_BINARY_KEYS:
+            smart = _smart_data(self.coordinator)
+            if smart is None:
+                return None
+            if self.entity_description.key == "smart_heat_needed_core":
+                return smart.heat_needed
+            if self.entity_description.key == "smart_heat_permitted_core":
+                return smart.heat_permitted
+            if self.entity_description.key == "smart_cooling_recommended_core":
+                return smart.cooling_recommended
+            if self.entity_description.key == "smart_fan_recommended_core":
+                return smart.fan_recommended
+            if self.entity_description.key == "smart_rising_too_fast_core":
+                return smart.rising_too_fast
         return self.entity_description.value_fn(self.coordinator.data)
