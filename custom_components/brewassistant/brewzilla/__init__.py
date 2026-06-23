68829a6f63807cf90c9bde8a15715da8048d6d24
Joachim Eriksson <sa0azy@gmail.com>
2026-06-23 22:59:08 +0200

    Load BrewZilla runtime safety wrapper

--- custom_components/brewassistant/brewzilla/__init__.py
+++ custom_components/brewassistant/brewzilla/__init__.py
@@ -1,5 +1,7 @@
 """BrewAssistant BrewZilla package."""
 
 from . import brewzilla_freshness_guard as _freshness_guard
+from . import brewzilla_stale_safe_guard as _runtime_safety
 
 _freshness_guard.install_freshness_guard()
+_runtime_safety.install_stale_safe_guard()
