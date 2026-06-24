04846213c9943277a438edee1688a508fe3d8e4b
Joachim Eriksson <sa0azy@gmail.com>
2026-06-24 22:35:08 +0200

    Install runtime gate

--- custom_components/brewassistant/brewzilla/__init__.py
+++ custom_components/brewassistant/brewzilla/__init__.py
@@ -2,6 +2,8 @@
 
 from . import brewzilla_freshness_guard as _freshness_guard
 from . import brewzilla_stale_safe_guard as _runtime_safety
+from . import brewzilla_execution_guard as _gate
 
 _freshness_guard.install_freshness_guard()
 _runtime_safety.install_stale_safe_guard()
+_gate.install_execution_guard()
