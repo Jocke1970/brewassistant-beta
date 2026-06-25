fa4441f5120dd1d334ac9e81f6f43131fcbe7c78
Joachim Eriksson <sa0azy@gmail.com>
2026-06-25 22:48:53 +0200

    Install mash ramp strategy

--- custom_components/brewassistant/brewzilla/__init__.py
+++ custom_components/brewassistant/brewzilla/__init__.py
@@ -1,9 +1,11 @@
 """BrewAssistant BrewZilla package."""
 
+from . import brewzilla_mash_ramp_strategy as _mash_ramp
 from . import brewzilla_freshness_guard as _freshness_guard
 from . import brewzilla_stale_safe_guard as _runtime_safety
 from . import brewzilla_execution_guard as _gate
 
+_mash_ramp.install_mash_ramp_strategy()
 _freshness_guard.install_freshness_guard()
 _runtime_safety.install_stale_safe_guard()
 _gate.install_execution_guard()
