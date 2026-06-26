b3f95f016b680dcf8567c4ad5d0f6a7f479feffc
Joachim Eriksson <sa0azy@gmail.com>
2026-06-26 21:34:44 +0200

    Install paused runtime guard

--- custom_components/brewassistant/brewzilla/__init__.py
+++ custom_components/brewassistant/brewzilla/__init__.py
@@ -3,6 +3,7 @@
 from . import brewzilla_mash_ramp_strategy as _mash_ramp
 from . import brewzilla_freshness_guard as _freshness_guard
 from . import brewzilla_stale_safe_guard as _runtime_safety
+from . import brewzilla_paused_guard as _paused_guard
 from . import brewzilla_execution_guard as _gate
 from .brewzilla_temp_filter import install_temp_filter as _install_temp
 
@@ -10,4 +11,5 @@ _mash_ramp.install_mash_ramp_strategy()
 _install_temp()
 _freshness_guard.install_freshness_guard()
 _runtime_safety.install_stale_safe_guard()
+_paused_guard.install_paused_guard()
 _gate.install_execution_guard()
