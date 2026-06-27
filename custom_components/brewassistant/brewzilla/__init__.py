3064c82acacb43710b09c6023c89957145af15a8
Joachim Eriksson <sa0azy@gmail.com>
2026-06-27 11:19:26 +0200

    Install BrewZilla no-positive gate last

--- custom_components/brewassistant/brewzilla/__init__.py
+++ custom_components/brewassistant/brewzilla/__init__.py
@@ -6,6 +6,7 @@ from . import brewzilla_freshness_guard as _freshness_guard
 from . import brewzilla_stale_safe_guard as _runtime_safety
 from . import brewzilla_paused_guard as _paused_guard
 from . import brewzilla_execution_guard as _gate
+from . import brewzilla_no_positive_gate as _no_positive_gate
 from .brewzilla_temp_filter import install_temp_filter as _install_temp
 
 _mash_ramp.install_mash_ramp_strategy()
@@ -15,3 +16,4 @@ _freshness_guard.install_freshness_guard()
 _runtime_safety.install_stale_safe_guard()
 _paused_guard.install_paused_guard()
 _gate.install_execution_guard()
+_no_positive_gate.install_no_positive_gate()
