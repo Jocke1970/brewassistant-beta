77010c87ab13863d2e1c78e243b4123f50224e88
Joachim Eriksson <sa0azy@gmail.com>
2026-09-24 22:15:24 +0200

    GF30: export read-only thermal preflight helper

--- custom_components/brewassistant/grainfather_fermenter/__init__.py
+++ custom_components/brewassistant/grainfather_fermenter/__init__.py
@@ -1,5 +1,9 @@
 """Grainfather fermentation-hardware adapter package."""
 
 from .adapter import build_grainfather_fermenter_snapshot
+from .thermal import build_manual_preflight_snapshot
 
-__all__ = ["build_grainfather_fermenter_snapshot"]
+__all__ = [
+    "build_grainfather_fermenter_snapshot",
+    "build_manual_preflight_snapshot",
+]
